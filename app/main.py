36e5b2fb30c71726e45813ededb0b28300142af1
Suradid Chao <chillshyld@gmail.com>
2020-09-17 19:14:54 +0700

    remove unused blog

--- app/main.py
+++ app/main.py
@@ -1,18 +1,14 @@
-from fastapi import FastAPI, Header, HTTPException, Depends,  Response
+from fastapi import FastAPI, Header, HTTPException, Depends, Response
 from pymongo import MongoClient
 
 
-from entities.blog import Blog
 from entities.book import Book
-from repositories.blog import BlogRepository, BlogCreatePayload, BlogUpdatePayload
 from repositories.book import BookRepository, NoBookError
 from routes.book import book_router
-from routes.blog import blog_router
 
 
 app = FastAPI()
 client = MongoClient("mongodb://cwdb101:!cwdb101!@localhost:27020")
-blog_repo = BlogRepository(client["bootcamp"]["blogs"])
 book_repo = BookRepository(client["bootcamp"]["books"])
 
 
@@ -23,15 +19,9 @@ def read_root():
 
 def get_token_header(api_key: str = Header(...)):
     if api_key != "complete-web-developer-bootcamp-2020":
-        raise HTTPException(status_code=400, detail="X-Token header invalid")
+        raise HTTPException(
+            status_code=400, detail="api-key header invalid")
 
-
-app.include_router(
-    blog_router(blog_repo),
-    prefix="/blogs",
-    tags=["blogs"],
-    dependencies=[Depends(get_token_header)],
-)
 
 app.include_router(
     book_router(book_repo),
